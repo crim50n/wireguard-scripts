052d5f1959f987383858afdc5853c320248158a0
crim50n <crims0n@yandex.ru>
2020-02-27 09:37:24 +0300

    Add files via upload

--- wireguard-centos.sh
+++ wireguard-centos.sh
@@ -38,15 +38,15 @@ cat <<'EOF' > add-client.sh
 #!/bin/bash
 
 # We read from the input parameter the name of the client
-if [ -z "$1" ]
-  then 
-    read -p "Enter VPN user name: " USERNAME
-    if [ -z $USERNAME ]
-      then
-      echo "[#]Empty VPN user name. Exit"
-      exit 1;
-    fi
-  else USERNAME=$1
+if [ -z "$1" ]	
+	then 
+		read -p "Enter VPN user name: " USERNAME
+		if [ -z $USERNAME ]
+			then
+			echo "[#]Empty VPN user name. Exit"
+			exit 1;
+		fi
+	else USERNAME=$1
 fi
 
 cd /etc/wireguard/
